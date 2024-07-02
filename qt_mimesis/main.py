c5008feb7eb8c777da0b8bdca7bd7af9d05eae5e
IginAndrew <asigin79@gmail.com>
2024-07-02 14:36:27 +0300

    добавляем таблицу credits в БД из формы

--- qt_mimesis/main.py
+++ qt_mimesis/main.py
@@ -1,7 +1,7 @@
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QMainWindow, QMessageBox
 
-from qt_mimesis.bd import Server, User, Txt_db, Email, Phone, Car
+from qt_mimesis.bd import Server, User, Txt_db, Email, Phone, Car, Credit
 from qt_mimesis.ui.ui_main import Ui_MainWindow
 
 
@@ -113,6 +113,17 @@ class Main(QMainWindow):
             if self.ui.lineEdit_5.text() !=  "":
                 s = self.add_database_name()
                 s.car(Car())
+            else:
+                return None
+        except:
+            return None
+
+# добавляем таблицу credits в БД
+    def add_table_to_bd_credits(self):
+        try:
+            if self.ui.lineEdit_6.text() !=  "":
+                s = self.add_database_name()
+                s.credit(Credit())
             else:
                 return None
         except:
